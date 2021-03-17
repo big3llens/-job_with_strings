8eb8eba08b036b2fe544f90aa28c1931716765cd
За Орду <markelovstass.93@mail.ru>
2021-03-17 18:12:56 +0300

    add project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         App app = new App();
-        app.doMethod("3[qwerty]2[PP2[OO]]");
-
-
-
-//        System.out.println(app.isValidParentheses("3[xf2[lkjb]gjf]2[dfgh5[zdfh]]"));
-//        System.out.println(app.isValidChars("3[xfgjf]2[dfgh5[zdfh]]"));
-//        char ch = 'k';
-//        String s = "qwerty";
-//        System.out.println(s.substring(3, 4));
-//        System.out.println(Character.isDigit(ch));
-//        StringBuilder sb = new StringBuilder("zdfhfh");
-//        System.out.println(sb.toString());
+        app.doMethod("3[qwerty3[YY]]2[PP2[OO]]");
     }
 }
